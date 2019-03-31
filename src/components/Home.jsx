276a0df504b1db12ff42605101a8a55641c6496f
Mark Mbugua <mbuguamark@gmail.com>
2019-03-31 12:07:27 +0300

    Style Home component

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -12,10 +12,31 @@ export default class Home extends Component {
                 <h2>Welcome to Infispace</h2>
                 <p>We are your number 1 software plugs. We stick to the
                     rules of the game.</p>      
+              <Link to="/about">
+                  <Button bsStyle="primary">Learn More</Button>
+              </Link>
             </Jumbotron>
-            <Link to="/about">
-                <Button bsStyle="primary">About</Button>
-            </Link>
+            <Row className="show-grid text-center">
+              <Col xs={12} sm={4} className="person-wrapper">
+                <Image src="assets/CEO.jpg" className="profile-pic"/>
+                <h3>Markize</h3>
+                <p>I am the CEO. It is my responsibility to see that we grow exponentially as well as 
+                  tapping into the amazing tech talents in the region</p>
+              </Col>
+              <Col xs={12} sm={4} className="person-wrapper">
+                <Image src="assets/CEO.jpg" className="profile-pic"/>
+                <h3>Kizako</h3>
+                <p>I am the Chariman of the board. It is my responsibility to see that we grow exponentially as well as 
+                  tapping into the amazing tech talents in the region</p>
+              </Col>
+              <Col xs={12} sm={4} className="person-wrapper">
+                <Image src="assets/CEO.jpg" className="profile-pic"/>
+                <h3>Danny</h3>
+                <p>I am the Company Administrator. It is my responsibility to see that we grow exponentially as well as 
+                  tapping into the amazing tech talents in the region</p>
+              </Col>
+            </Row>
+            
         </Container>
     )
   }
