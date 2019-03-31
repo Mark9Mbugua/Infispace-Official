dc60a4569f2fc68d7a03ecbf59d61455893a94c9
Mark Mbugua <mbuguamark@gmail.com>
2019-03-31 03:30:11 +0300

    Add components to App

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
-import { link } from 'react-roter-dom';
-import { Jumbotron, Grid, Row, Col, Image, Button } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import { Jumbotron, Container, Row, Col, Image, Button } from 'react-bootstrap';
 import './Home.css';
 
 export default class Home extends Component {
   render() {
     return (
-        <Grid>
+        <Container>
             <Jumbotron>
                 <h2>Welcome to Infispace</h2>
                 <p>We are your number 1 software plugs. We stick to the
@@ -15,7 +15,7 @@ export default class Home extends Component {
             <Link to="/about">
                 <Button bsStyle="primary">About</Button>
             </Link>
-        </Grid>
+        </Container>
     )
   }
 }
